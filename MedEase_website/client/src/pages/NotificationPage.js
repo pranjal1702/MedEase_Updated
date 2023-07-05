b09bf9f317150c8e494749f5573717ebde607e18
pranjal1702 <101694156+pranjal1702@users.noreply.github.com>
2023-07-05 14:43:33 +0530

    Update NotificationPage.js

--- MedEase_website/client/src/pages/NotificationPage.js
+++ MedEase_website/client/src/pages/NotificationPage.js
@@ -15,7 +15,7 @@ const NotificationPage = () => {
     try {
       dispatch(showLoading());
       const res = await axios.post(
-        "/api/v1/user/get-all-notification",
+        "https://medease-hms.onrender.com/api/v1/user/get-all-notification",
         {
           userId: user._id,
         },
@@ -43,7 +43,7 @@ const NotificationPage = () => {
     try {
       dispatch(showLoading());
       const res = await axios.post(
-        "/api/v1/user/delete-all-notification",
+        "https://medease-hms.onrender.com/api/v1/user/delete-all-notification",
         { userId: user._id },
         {
           headers: {
