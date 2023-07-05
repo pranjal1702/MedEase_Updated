caefd12e174724dd0c969b1de6def5f51e74b25d
pranjal1702 <101694156+pranjal1702@users.noreply.github.com>
2023-07-05 14:42:57 +0530

    Update Login.js

--- MedEase_website/client/src/pages/Login.js
+++ MedEase_website/client/src/pages/Login.js
@@ -12,7 +12,7 @@ const Login = () => {
   const onfinishHandler = async (values) => {
     try {
       dispatch(showLoading());
-      const res = await axios.post("/api/v1/user/login", values);
+      const res = await axios.post("https://medease-hms.onrender.com/api/v1/user/login", values);
       window.location.reload();
       dispatch(hideLoading());
       if (res.data.success) {
