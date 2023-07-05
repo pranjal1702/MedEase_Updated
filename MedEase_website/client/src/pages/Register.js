3e090bcb96b9fababd8e8f12a3f98074ea4b2f0f
pranjal1702 <101694156+pranjal1702@users.noreply.github.com>
2023-07-05 14:44:06 +0530

    Update Register.js

--- MedEase_website/client/src/pages/Register.js
+++ MedEase_website/client/src/pages/Register.js
@@ -14,7 +14,7 @@ const Register = () => {
   const onFinishHandler = async (values) => {
     try {
       dispatch(showLoading());
-      const res = await axios.post("/api/v1/user/register", values);
+      const res = await axios.post("https://medease-hms.onrender.com/api/v1/user/register", values);
       dispatch(hideLoading());
       if (res.data.success) {
         message.success("Register Successfully!");
