400f7ed72848db6a67b1d56296a4f446f7313936
pranjal1702 <101694156+pranjal1702@users.noreply.github.com>
2023-07-05 14:48:15 +0530

    Update Profile.js

--- MedEase_website/client/src/pages/doctor/Profile.js
+++ MedEase_website/client/src/pages/doctor/Profile.js
@@ -19,7 +19,7 @@ const Profile = () => {
     try {
       dispatch(showLoading());
       const res = await axios.post(
-        "/api/v1/doctor/updateProfile",
+        "https://medease-hms.onrender.com/api/v1/doctor/updateProfile",
         {
           ...values,
           userId: user._id,
@@ -53,7 +53,7 @@ const Profile = () => {
   const getDoctorInfo = async () => {
     try {
       const res = await axios.post(
-        "/api/v1/doctor/getDoctorInfo",
+        "https://medease-hms.onrender.com/api/v1/doctor/getDoctorInfo",
         { userId: params.id },
         {
           headers: {
